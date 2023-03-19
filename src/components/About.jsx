5a13e9f61b5764e78bdb117542be040a840d7b5b
hazrath <hazrath@localhost.localdomain>
2023-03-19 22:25:48 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-primary text-[17px] max-w-3xl leading-[30px]'
+      >
+       Welcome to my portfolio website! I'm Hazrath, a dynamic full-stack developer with extensive experience in Python (Django), JavaScript (React, React Native, Express, Node.js, and MongoDB), and Figma design.
+
+I have a passion for building high-performing applications, both large and small, and thrive on solving complex problems for my clients. I stay at the forefront of the latest technologies and design trends, constantly expanding my knowledge and skillset to deliver cutting-edge solutions.
+
+In addition to my programming expertise, I'm also skilled in using Figma to create beautiful, intuitive designs that prioritize user experience. Please explore my portfolio to view my latest work and get in touch if you're interested in collaborating on a project. I look forward to hearing from you!</motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
